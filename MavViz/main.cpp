3462fb80c5fc9f30c40588842733ab030e79d6c8
Jason Everett <jme5297@psu.edu>
2018-09-26 15:55:12 -0500

    Only working with one VBO/VAO

--- MavViz/main.cpp
+++ MavViz/main.cpp
@@ -4,7 +4,6 @@
 
 int main()
 {
-
 	GLFWwindow * window = MavViz::Graphics::initGLWindow(800, 600);
 
 	float myTriangle[] = {
@@ -34,8 +33,6 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(myTriangle), myTriangle, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
 
 	/* Main render loop */
 	while (!glfwWindowShouldClose(window))
